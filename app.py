bd79a416c8f53e51685a2024f90a80e2b04c8873
lsvvt <losev.tim.v@gmail.com>
2022-03-11 20:00:04 +0300

    make it better

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def send(msg):
         bot.send_message(chat_id, msg, parse_mode='HTML')
 
 def get_users(user_info):
-    return user_info["ID"]
+    return user_info["id"]
 
 
 def gen_message_from_tasks(tasks, task_id):
